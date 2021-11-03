87b33de6cfb547bf0b7f4dbee84eeb9aab35c07f
arslanarjumand012 <arslanarjumand012@gmail.com>
2021-11-04 02:46:18 +0500

    Updates

---
+++ precache-manifest.b49be660ab35e59287c7d207a58e1081.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b721114a50e9e7359bc2521c0f172a89",
+    "url": "/Message-Chat-App-ReactJs/index.html"
+  },
+  {
+    "revision": "14d486160149512cb0db",
+    "url": "/Message-Chat-App-ReactJs/static/css/2.f7861fd5.chunk.css"
+  },
+  {
+    "revision": "d0951301e597d8e8c8ea",
+    "url": "/Message-Chat-App-ReactJs/static/css/main.51c6ebb1.chunk.css"
+  },
+  {
+    "revision": "14d486160149512cb0db",
+    "url": "/Message-Chat-App-ReactJs/static/js/2.5f3eed8f.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/Message-Chat-App-ReactJs/static/js/2.5f3eed8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0951301e597d8e8c8ea",
+    "url": "/Message-Chat-App-ReactJs/static/js/main.c1ecea35.chunk.js"
+  },
+  {
+    "revision": "4dfd7b8950e4ed7e2949",
+    "url": "/Message-Chat-App-ReactJs/static/js/runtime-main.ca838eef.js"
+  }
+]);
